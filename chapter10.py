dad9862885e0a64e9d8909b7ddd747b4a5ef1ad8
EnriLion <enriqueleon0201leon@gmail.com>
2023-05-07 22:24:36 -0600

    Updating repo

--- chapter10.py
+++ chapter10.py
@@ -8,3 +8,41 @@ print(max(y))
 for iter in y:
     print(iter)
 
+print('but... Tuples are "immutable"')
+
+x = [9, 8, 7]
+x[2] = 6
+print(x)
+
+# y = 'ABC'
+# # y[2] ='D'
+
+# z =(5, 4, 3)
+# z[2] = 0
+
+print('Things not to do with tuples')
+x = (3, 2, 1)
+# x.sort()
+# x.append()
+# x.reverse()
+print('A tale of two Sequences')
+l = list()
+print(dir(l))
+print("\n")
+t = tuple()
+print(dir(t))
+print("\n")
+print('Tuples and Assignment')
+(x, y) = (4, 'fred')
+print(y)
+(a, b) = (99, 98)
+print(a)
+print("\n")
+print('Tuples and Dictionaries')
+d = dict()
+d['csev'] = 2
+d['cwen'] = 4
+for (k,v) in d.items():
+    print(k, v)
+tups = d.items()
+print(tups)
