b4276f0f1ee137d1207c8a1d0a0de30b965e3e61
EnriLion <enriqueleon0201leon@gmail.com>
2023-05-20 21:12:51 -0600

    Updating files

--- chapter11.py
+++ chapter11.py
@@ -11,3 +11,22 @@ print(y)
 x = 'From: Using the: character'
 y = re.findall('^F.+:',x)
 print(y)
+print("\nNo-Greedy Matching\n")
+#import re
+x = 'From: Using the: character'
+y = re.findall('^F.+?:',x)
+print(y)
+x = 'From stephen.marquez@uct.ac.za Sat Jan 5 09:14:16 2008'
+y = re.findall('\S+@\S+',x)
+print(y)
+print("\n")
+y = re.findall('^From (\S+@\S+)',x)
+print(y)
+print("\nAnother example\n")
+data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
+atpos = data.find('@')
+print(atpos)
+sppos = data.find(' ',atpos)
+print(sppos)
+host = data[atpos+1 : sppos]
+print(host)
