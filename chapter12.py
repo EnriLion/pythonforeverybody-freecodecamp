0b5ef2b42f508d692f031a90c8351a2f99d3b444
EnriLion <enriqueleon0201leon@gmail.com>
2023-06-21 21:29:40 -0600

    Updating files

---
+++ chapter12.py
@@ -0,0 +1,16 @@
+print("Representing simple strings")
+print(ord('H'))# 72
+print(ord('e'))# 101 e is lower tha H and that is why is higher
+print(ord('\n'))# 10
+print("Two Kinds of Strings in Python")
+x = '書簡'
+print(type(x))
+x = u'書簡'
+print(type(x)) # in previously if we wrote with u we could represent unite only in the version 2.7.10 technically string and unicode are the same
+print("\n")
+x = b'abc'
+print(type(x))
+x = '書簡'
+print(type(x))
+x = u'書簡'
+print(type(x))
--- code3/socket1.py
+++ code3/socket1.py
@@ -1,14 +1,14 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+mysock.connect(('data.pr4e.org', 80))# works with the telnet
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()#there are strings inside of Python that are Unicode(UTF-8)
+mysock.send(cmd) #with the socker while using a method we send the direction
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
+while True:# 
+    data = mysock.recv(512)#create a variable with recieve of 512 characters
+    if len(data) < 1: #if this is the end of the file we are going to stop with a break
         break
-    print(data.decode(),end='')
+    print(data.decode())# Converts into the unicode that runs inside
 
-mysock.close()
+mysock.close() #open a socket and retrieve the data
